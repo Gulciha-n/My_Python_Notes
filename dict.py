5388196f99fd18c7beda6656ebb97780595a6d7e
Gülcihan <gulmezgulcihan44@gmail.com>
2023-07-03 10:27:24 +0300

    Dict_exp

---
+++ dict.py
@@ -0,0 +1,21 @@
+'''inventory = dict()
+inventory["bananas"] = 120
+inventory["oranges"] = 35
+inventory["apples"] = 40
+
+for key in inventory:
+    inventory[key] += 100
+    print(key)
+
+for key,value in inventory.items():
+    if value > 200:
+        print("too many",key)
+    else:
+        print("enough",key)'''
+
+
+
+
+
+
+    
