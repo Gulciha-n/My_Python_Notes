717058aa10b09dbefb3cf12483b5890f1c01bc52
Gülcihan <gulmezgulcihan44@gmail.com>
2023-07-03 10:23:30 +0300

    Module

---
+++ module.py
@@ -0,0 +1,21 @@
+'''def area_round(r):
+    return 3.14 * r * r 
+
+def perimeter_circle(r):
+    return 2*3.14*r
+
+result = area_round(5)
+print(result)
+'''
+
+inventory = dict()
+inventory["bananas"]= 40
+inventory["apples"]=28
+inventory["oranges"]=35
+
+for key in inventory:
+    inventory[key] += 100
+
+print(inventory)
+
+
