2beda80304542b4688cf58c6a53578f28ffe7c31
Gülcihan <gulmezgulcihan44@gmail.com>
2023-07-04 11:59:05 +0300

    class_exp8

---
+++ class_exp8.py
@@ -0,0 +1,9 @@
+age = 19
+worker = "pelin" "'s age is :"  + str(age)
+print (worker)
+
+city = 'Boston'
+index = 0
+while index < 7:           #index eroor (out of range)
+    print(city[index])
+    index += 1
