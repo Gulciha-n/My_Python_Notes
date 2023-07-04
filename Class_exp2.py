32cd7d1e83e5bba5b69cbe14d5fbeac12d1baf68
Gülcihan <gulmezgulcihan44@gmail.com>
2023-07-04 11:42:34 +0300

    Class_exp2

---
+++ Class_exp2.py
@@ -0,0 +1,13 @@
+class Employee:
+   def __init__(self, a, b, c):
+      self.name = a
+      self.surname = b
+      self.age = c
+
+employee1 = Employee("batu", "gülmez", 20)
+print(employee1.name, employee1.surname, employee1.age)
+    
+
+
+
+
