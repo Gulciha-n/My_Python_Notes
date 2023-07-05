03937e368a5d82bcd21b375dcdd18363085a2b0e
Gülcihan <gulmezgulcihan44@gmail.com>
2023-07-05 19:23:50 +0300

    Loop

---
+++ loop.py
@@ -0,0 +1,9 @@
+names = ["derya" , "pelin" , "cansu","suna","5",5]
+
+for name in names:
+    if type(name) != str:
+        print ("found",name)
+        break
+    else:
+         print(name,"is a string")
+
